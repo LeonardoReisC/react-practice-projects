e7d1901a38d7da6d23ee187672c22ae01b6783b0
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-01-31 15:58:10 -0300

    build navbar

--- react-facts/src/components/Navbar.jsx
+++ react-facts/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import logo from "../assets/images/react.svg";
 
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>;
+    return (
+        <nav className="navbar">
+            <img className="nav--icon" src={logo} alt="React Logo" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    );
 }
