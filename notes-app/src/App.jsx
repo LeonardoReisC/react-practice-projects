db9fe629b3f701235709ad84272eb099c9abecd1
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-10 20:21:02 -0300

    notes-app: sync notes with localStorage

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -6,10 +6,14 @@ import { nanoid } from "nanoid";
 import "./App.css";
 
 export default function App() {
-    const [notes, setNotes] = React.useState([]);
+    const [notes, setNotes] = React.useState(
+        () => JSON.parse(localStorage.getItem("notes")) ?? []
+    );
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     );
+
+    React.useEffect(() => localStorage.setItem("notes", JSON.stringify(notes)));
 
     function createNewNote() {
         const newNote = {
