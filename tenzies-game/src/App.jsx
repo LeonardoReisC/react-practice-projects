88132019cd84d3c98a69580f93545d0913d53f65
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-02-13 16:15:38 -0300

    tenzies-game: generate array of 10 random numbers

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -3,6 +3,15 @@ import Die from "./components/Die";
 import "./App.css";
 
 function App() {
+    function allNewDice() {
+        let randomNumbersArray = [];
+        for (let i = 0; i < 10; i++) {
+            const dieNumber = Math.ceil(Math.random() * 6);
+            randomNumbersArray.push(dieNumber);
+        }
+        return randomNumbersArray;
+    }
+
     return (
         <main>
             <div className="main--dice">
