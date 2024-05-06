6507c0b9b8f195f77ce1239b1f2f1720211205a4
Leonardo Reis Coimbra <coimbra.lreis@gmail.com>
2024-05-06 20:55:51 -0300

    notes-app: encapsulate Firebase configs

--- notes-app/src/firebase.js
+++ notes-app/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApGcJh1EOOB0c7EEhk00QPyUW50u3nWFg",
-    authDomain: "react-notes-45819.firebaseapp.com",
-    projectId: "react-notes-45819",
-    storageBucket: "react-notes-45819.appspot.com",
-    messagingSenderId: "746916515859",
-    appId: "1:746916515859:web:9ec0c1f007a9d184adefe6",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
